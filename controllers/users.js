9f074c3a8f739416503c823c83a406147ba08d4f
Yuliya-Lov <jul-yaris@yandex.ru>
2023-07-18 11:23:55 +0300

    fix: user

--- controllers/users.js
+++ controllers/users.js
@@ -1,104 +1,100 @@
+const mongoose = require('mongoose');
 const User = require('../models/user');
 
-class UserError extends Error {
-  constructor(m) {
-    super(m);
-    this.message = {
-      message: m,
-    };
-  }
+const UserNotFoundError = new Error('Пользователь с указанным _id не найден');
+UserNotFoundError.name = 'UserNotFoundError';
 
-  getProperties() {
-    if (this.message.message === 'Переданы некорректные данные') {
-      this.name = 'UncorrectDataUserError';
-      this.statusCode = 400;
-      return;
-    }
-    if ((this.message.message === 'Пользователь по указанному _id не найден') || this.message.message.toString().startsWith('CastError: Cast to ObjectId failed for value')) {
-      this.name = 'UserNotFoundError';
-      this.statusCode = 404;
-      return;
-    }
-    this.message.message = 'Произошла ошибка запроса данных пользователя';
-    this.name = 'DefaultUserError';
-    this.statusCode = 500;
-  }
-
-  indicateErr() {
-    console.log(`Ошибка: ${this.name}, код ошибки: ${this.statusCode}`);
-  }
-}
-
-const userError = (message) => {
-  const err = new UserError(message);
-  err.getProperties();
-  return err;
-};
+const UncorrectDataUserError = new Error('Переданы некорректные данные');
+UncorrectDataUserError.name = 'UncorrectDataUserError';
 
 const getAllUsers = (req, res) => {
   User.find({})
     .then((users) => {
       res.send({ data: users });
     })
-    .catch((m = '') => {
-      userError(m).indicateErr();
-      res.status = userError(m).statusCode;
-      res.send(userError(m).message);
+    .catch(() => {
+      res.status(500).send({ message: 'Произошла ошибка запроса данных пользователя' });
     });
 };
 
 const getUser = (req, res) => {
-  User.findById(req.params.id)
+  (mongoose.Types.ObjectId.isValid(req.params.id)
+    ? User.findById(req.params.id)
+    : Promise.reject(UncorrectDataUserError))
     .then((user) => {
-      res.send({ data: user })
+      user
+        ? res.send({ data: user })
+        : res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
     })
-    .catch((m = '') => {
-      userError(m).indicateErr();
-      res.status = userError(m).statusCode;
-      res.send(userError(m).message);
+    .catch((err) => {
+      if (err.name === 'UncorrectDataUserError') {
+        res.status(400).send({ message: `${err.message}` });
+      } else if (err.name === 'CastError') {
+        res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка запроса данных пользователя' });
+      }
     });
 };
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
-  ((!!name && !!about && !!avatar)
+  (mongoose.Types.ObjectId.isValid(name)
     ? User.create({ name, about, avatar })
-    : Promise.reject('Переданы некорректные данные'))
+    : Promise.reject(UncorrectDataUserError))
     .then((user) => {
-      res.send({ data: user });
+      user
+        ? res.send({ data: user })
+        : res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
     })
-    .catch((m = '') => {
-      userError(m).indicateErr();
-      res.status = userError(m).statusCode;
-      res.send(userError(m).message);
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка запроса данных пользователя' });
+      }
     });
 };
 
 const updateUserInfo = (req, res) => {
   const { name, about } = req.body;
-  ((!!name || !!about)
-    ? User.findByIdAndUpdate(req.user._id, { name, about }, {
-      new: true,
-      upsert: true,
-    })
-    : Promise.reject('Переданы некорректные данные'))
+  User.findByIdAndUpdate(req.user._id, { name, about }, {
+    new: true,
+    upsert: true,
+    runValidators: true,
+  })
     .then((user) => res.send({ data: user }))
-    .catch((m = '') => {
-      userError(m).indicateErr();
-      res.status = userError(m).statusCode;
-      res.send(userError(m).message);
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(404).send({ message: 'Пользователь с указанным _id не найден' })
+      }
+      else if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      }
+      else {
+        res.status(500).send({ message: 'Произошла ошибка запроса данных пользователя' });
+      }
     });
 };
 
 const updateUserAvatar = (req, res) => {
   (req.body.avatar
-    ? User.findByIdAndUpdate(req.user._id, { avatar: req.body.avatar })
-    : Promise.reject('Переданы некорректные данные'))
+    ? User.findByIdAndUpdate(req.user._id,
+      { avatar: req.body.avatar },
+      {
+        new: true,
+        upsert: true,
+        runValidators: true,
+      },
+    )
+    : Promise.reject(UncorrectDataUserError))
     .then((user) => res.send({ data: user }))
-    .catch((m = '') => {
-      userError(m).indicateErr();
-      res.status = userError(m).statusCode;
-      res.send(userError(m).message);
+    .catch((err) => {
+      if (err.name === 'UncorrectDataUserError') {
+        res.status(400).send({ message: `${err.message}` });
+      } else {
+        res.status(500).send({ message: 'Произошла ошибка запроса данных пользователя' });
+      }
     });
 };
 
--- models/user.js
+++ models/user.js
@@ -6,12 +6,24 @@ const userSchema = new mongoose.Schema({
     required: true,
     minlength: 2,
     maxlength: 30,
+    validate: {
+      validator(v) {
+        return (v.length >= 2 && v.length <= 30);
+      },
+      message: 'Введите имя от 2 до 30 символов!',
+    },
   },
   about: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 30,
+    validate: {
+      validator(v) {
+        return (v.length >= 2 && v.length <= 30);
+      },
+      message: 'Введите описание от 2 до 30 символов!',
+    },
   },
   avatar: {
     type: String,
